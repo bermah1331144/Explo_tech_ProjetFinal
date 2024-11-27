e9a746f691d0a69bfa875bb10aab378dd36ab0ba
Mahelie Bergeron <1331144@etu.cegepjonquiere.ca>
2024-11-27 10:32:00 -0500

    ajouter la page de stats tempeFoid et tempeChaud

--- temperama/src/Controller/AccueilController.php
+++ temperama/src/Controller/AccueilController.php
@@ -14,6 +14,47 @@ class AccueilController extends AppController
         $temperature = $temperaturesTable->find()->first();
         $this->set(compact('temperature'));
     }
+    
+    public function GetTemperatureFroide()
+    {
+        $temperatureFroide = $this->Temperature->find('all',['condition'=>['temperature<' => 0], 'fields' =>['tempeFroid']
+        ]);
+        $this->set(compact('tempeFroid',$temperatureFroide));
+    
+    }
+
+    public function GetTemperatureChaude(){
+
+        $temperatureChaude =$this->temperature->find('all',['condition'=> ['temperature<'=> 35], 'fiels'=>['tempeChaud']
+        ]);
+        $this->set(compact('tempeChaud', $temperatureChaude));
+
+    }
+
+    public function GetTouteTemperature(){
+        $this->set(compact('temperatureFroide','temperatureChaude'));
+    }
+
+
+
+
+   public function AfficherTableau($type){
+        $tableauFroid = document.getElementById('tableau-froid-contrainer');
+        $tableauChaud = document.getElementById('tableau-chaud-contrainer');
+        
+        $tableauFroid.style.display == 'none';
+        $tableauChaud.style.display =='none';
+
+        if(type == 'froid'){
+            $tableauFroid.style.display == 'block';
+            
+        }else if(type == 'chaud'){
+            $tableauChaud.style.display == 'block';
+        }
+   }
+
+
+
 }
 ?>
 
